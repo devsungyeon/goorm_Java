886ea5be423b152b0f40a67f8b6ddc7c61ddd3b2
devsungyeon <sungyeonlee1103@gmail.com>
2021-03-26 08:01:10 +0000

    ing

--- src/project/pgtwoadd.java
+++ src/project/pgtwoadd.java
@@ -1,13 +1,33 @@
 package project;
+import java.util.*;
 
-public class pgtwoadd {
-
+public class pgtwoadd {        
+    public int[] solution(int[] numbers) {
+        int[] answer = {};
+        List<Integer> arrList = new ArrayList<>();
+        
+        for(int i = 0; i<numbers.length ; i++ ) {   
+            for(int j = i+1; j<numbers.length ; j++ ) {
+                arrList.add(numbers[i]+numbers[j]);
+            }
+        }
+        
+        return answer;
+    }
     /**
      * @param args
      */
     public static void main(String[] args) {
         // TODO Auto-generated method stub
-        System.out.println("Hello test!");
+        int[] array = {2,1,3,4,1};
+        int[] ans = {};
+        ans = solution(array);
+        
+        for(int outp : ans) {
+            System.out.println(outp);
+        }
+        
+        //System.out.println("Hello test!");
     }
 
 }
