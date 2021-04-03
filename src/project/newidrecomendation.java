48f9c1ef2e2593cfc63e4a596cb97c95b2e4ada9
devsungyeon <sungyeonlee1103@gmail.com>
2021-04-03 22:22:03 +0900

    newidrecommendation created

---
+++ src/project/newidrecomendation.java
@@ -0,0 +1,25 @@
+package project;
+
+class Solutionn {
+    public String solution(String new_id) {
+        String answer = "";
+        System.out.println(new_id);
+
+        new_id = new_id.toLowerCase();
+        System.out.println("1 : " + new_id);
+
+        new_id.
+
+
+        return answer;
+    }
+}
+
+public class newidrecomendation {
+    public static void main(String[] args) {
+        String new_id = "...!@BaT#*..y.abcdefghijklm" ;
+        Solutionn a = new Solutionn();
+        String ans = a.solution(new_id);
+        System.out.println(ans);
+    }
+}
